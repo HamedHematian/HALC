35a7da363e62a0395d7b8ba5678fea4899004316
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-05-29 18:51:36 -0500

    Update llava_llama.py

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -162,5 +162,5 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
         )
         return model_inputs
 
-AutoConfig.register("llavai", LlavaConfig)
+AutoConfig.register("llava", LlavaConfig)
 AutoModelForCausalLM.register(LlavaConfig, LlavaLlamaForCausalLM)
