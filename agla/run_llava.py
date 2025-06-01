103dc255d08dfcce8055a8051ec84d27d4671e86
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-06-01 18:51:27 -0500

    Update run_llava.py

--- agla/run_llava.py
+++ agla/run_llava.py
@@ -112,11 +112,11 @@ def eval_model(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-path", type=str, default="/workspace/model/llava_1.5_7b")
+    parser.add_argument("--model-path", type=str, default="liuhaotian/llava-v1.5-7b")
     parser.add_argument("--model-base", type=str, default=None)
-    parser.add_argument("--image-folder", type=str, default="/workspace/data/val2014")
-    parser.add_argument("--question-file", type=str, default="/workspace/10_AGLA/data/POPE/coco/coco_pope_adversarial.json")
-    parser.add_argument("--answers-file", type=str, default="/workspace/10_AGLA/eval/output/test.jsonl")
+    parser.add_argument("--image-folder", type=str, default="../data/val2014")
+    parser.add_argument("--question-file", type=str, default="../data/POPE/coco/coco_pope_adversarial.json")
+    parser.add_argument("--answers-file", type=str, default="output/test.jsonl")
     parser.add_argument("--conv-mode", type=str, default="llava_v1")
     parser.add_argument("--num-chunks", type=int, default=1)
     parser.add_argument("--chunk-idx", type=int, default=0)
