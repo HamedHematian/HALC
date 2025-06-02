3cea08ad8ed6906ef269ab1f517ead19ef49674b
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-06-02 11:47:13 -0500

    Update sample.py

--- agla/sample.py
+++ agla/sample.py
@@ -128,7 +128,7 @@ def sample(
         model_kwargs_cd = model_kwargs.copy()
 
         if use_cd:
-
+            print('I AM USING CD ------------------')
             model_inputs_cd = self.prepare_inputs_for_generation_cd(input_ids, **model_kwargs_cd)
             outputs_cd = self(
                 **model_inputs_cd,
