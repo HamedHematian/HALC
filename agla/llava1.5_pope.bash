4cc312f541d6c8015b8c90332d345efcb5c75906
Hamed Hematian <70624162+HamedHematian@users.noreply.github.com>
2025-06-02 08:47:39 -0500

    Update llava1.5_pope.bash

--- agla/llava1.5_pope.bash
+++ agla/llava1.5_pope.bash
@@ -1,16 +1,23 @@
 dataset_name='coco' # 'aokvqa' 'gqa'
 image_folder='../data/val2014' # '/workspace/data/gqa'
-for seed in 1 2 3
+seed = 42343
+for alpha in .5 1. 1.5 2.
 do
-for type in 'random' 'popular' 'adversarial'
+for beta in .2 .4 .6
+do
+for type in 'random'
 do
 python run_llava.py \
 --question-file ../data/POPE/${dataset_name}/${dataset_name}_pope_${type}.json \
 --image-folder ${image_folder} \
---answers-file ../output/llava_${dataset_name}_pope_${type}_answers_agla_seed${seed}.jsonl \
+--answers-file ../output/llava_${dataset_name}_pope_${type}_answers_agla_seed${seed}_alpha_${alpha}_beta_${beta}.jsonl \
 --use_agla \
---alpha 2 \
---beta 0.5 \
+--alpha $alpha \
+--beta $beta \
 --seed $seed
+
+python eval_pope.py --gen_files ./output/llava_coco_pope_adversarial_answers_agla_seed1.jsonl
+
+done
 done
 done
